811c6c799483ce1fe26e93181930e1ab28289eea
Spiritecho <141818525+Spiritecho777@users.noreply.github.com>
2026-08-05 16:22:04 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,52 +1,40 @@
 const i18n = {
     fr: {
         title: "Mes Projets",
-
         tab_all: "Tous",
         tab_windows: "Windows",
         tab_linux: "Linux",
         tab_mac: "MacOS",
         tab_android: "Android",
         tab_web: "Web",
-
         win_title: "Projet Windows",
         win_desc: ".exe",
-
         linux_title: "Projet Linux",
         linux_desc: "Binaire",
-
         mac_title: "Projet MacOS",
         mac_desc: "Build Mac",
-
         android_title: "Projet Android",
         android_desc: "APK",
-
         web_title: "Projet Web",
         web_desc: "Site / WebApp"
     },
 
     en: {
         title: "My Projects",
-
         tab_all: "All",
         tab_windows: "Windows",
         tab_linux: "Linux",
         tab_mac: "MacOS",
         tab_android: "Android",
         tab_web: "Web",
-
         win_title: "Windows Project",
         win_desc: ".exe",
-
         linux_title: "Linux Project",
         linux_desc: "Binary",
-
         mac_title: "MacOS Project",
         mac_desc: "Mac Build",
-
         android_title: "Android Project",
         android_desc: "APK",
-
         web_title: "Web Project",
         web_desc: "Website / WebApp"
     }
@@ -70,7 +58,7 @@ function initLanguage() {
     applyLanguage(lang);
 }
 
-document.getElementById("lang").addEventListener("change", (e) => {
+document.getElementById("lang").addEventListener("change", e => {
     applyLanguage(e.target.value);
 });
 
