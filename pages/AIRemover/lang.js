4d01651f2e10bfe9e2dd1ade57742447262c8476
Spiritecho <141818525+Rona-code@users.noreply.github.com>
2026-09-11 13:02:32 +0200

    Update lang.js

--- pages/AIRemover/lang.js
+++ pages/AIRemover/lang.js
@@ -12,7 +12,9 @@ const translations = {
           <strong>Détection Shadow DOM</strong><br>
           <strong>Nettoyage en temps réel</strong><br>
           <strong>Léger & 100% privé</strong><br><br>
-        `
+        `,
+        before:"Avant",
+        after:"Après"
     },
 
     en: {
@@ -28,7 +30,9 @@ const translations = {
           <strong>Shadow DOM detection</strong><br>
           <strong>Real‑time clean‑up</strong><br>
           <strong>Lightweight & private</strong><br><br>
-        `
+        `,
+        before:"Before",
+        after:"After"
     }
 };
 
